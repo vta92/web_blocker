747511ce6ff2a3b8772427c7f222e4cdc14c19e6
Vinh Ta <vinhta92@gmail.com>
2017-07-21 01:41:22 -0700

    initializing a web blocker app project

---
+++ web_blocker.py
@@ -0,0 +1,7 @@
+#uses python3
+
+
+#directory and IP to use
+host_path = 'C:\Windows\System32\drivers\etc\hosts'
+redirected_ip = '127.0.0.1'
+webs_list = ['www.facebook.com', 'facebook.com', 'www.youtube.com', 'youtube.com']
